5b8f2ff7c4a642aefa64e6554e0d36edf15b015a
Костянтин Ємельянов <kostaemelanov30@gmail.com>
2022-12-27 14:38:18 +0200

    Переделал консольное меню, добавил незаполненные полностью массивы

--- Project1/add.h
+++ Project1/add.h
@@ -1,10 +1,37 @@
-#pragma once
+#ifndef ADD_H
+#define ADD_H
+#endif ADD_H
 #include<iostream>
-#include<string>
 using namespace std;
-
 class ConsoleMenu
 {
 public:
 	void Menu();
+};
+
+class InfoComponents
+{
+private:
+	
+	string info[12] =
+	{
+		"Wheel - ",
+		"Doors - ",
+		"‘ - "
+		"«еркала - ",
+		"“ело - ",
+		"ѕодвеска - "
+	};
+	string type[7][3] =
+	{ 
+		{"плохое", "среднее", "хорошее"},
+		{"плохое", "среднее", "хорошее"},
+		{"плохое", "среднее", "хорошее"},
+		{"плохое", "среднее", "хорошее"},
+		{"плохое", "среднее", "хорошее"},
+		{"плохое", "среднее", "хорошее"},
+		{"плохое", "среднее", "хорошее"}, 
+	};
+public:
+	
 };
