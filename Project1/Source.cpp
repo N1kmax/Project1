4bc5a19159263e6eccbed3c8aae5a46842bc6aa4
N1kmax <nkmax6032@gmail.com>
2022-12-27 11:49:35 +0100

    Добавил Header

--- Project1/Source.cpp
+++ Project1/Source.cpp
@@ -1,4 +1,5 @@
 #include<iostream>
+#include"add.h"
 using namespace std;
 
 
--- Project1/add.h
+++ Project1/add.h
@@ -1,1 +1,13 @@
-#pragma once
+#ifndef ADD_H
+#define ADD_H
+class car 
+{
+private:
+	int wheels, doors, lights, mirrors, body, weight;
+	string name, suspension, trunk, hood, engine, Transmission, salon, spoiler;
+public:
+	void input();
+	void ouput();
+};
+
+#endif
